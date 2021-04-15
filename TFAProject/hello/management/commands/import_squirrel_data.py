d9a58718f24f7d259a7e5b151d776144b1aa556f
sd3419 <sd3419@columbia.edu>
2021-04-15 12:41:06 -0400

    intendations fixed for import csv

--- TFAProject/hello/management/commands/import_squirrel_data.py
+++ TFAProject/hello/management/commands/import_squirrel_data.py
@@ -18,11 +18,16 @@ class Command(BaseCommand):
             for row in reader:
                 try:
 
-                    a = squirrel_data(row[0],row[1],row[2],row[3],row[4],datetime.datetime.strptime(row[5], '%m%d%Y').date(),row[6],row[7],row[8],
-                                      row[9],row[10],row[11],row[12],row[13],row[14],
-                                      row[15].capitalize(),row[16].capitalize(),row[17].capitalize(),row[18].capitalize(),row[19].capitalize(),
-                                      row[20],
-                                      row[21].capitalize(),row[22].capitalize(),row[23].capitalize(),row[24].capitalize(),row[25].capitalize(),row[26].capitalize(),
+                    a = squirrel_data(row[0],row[1],row[2],row[3],
+                                      row[4],datetime.datetime.strptime(row[5], '%m%d%Y').date(),
+                                      row[6],row[7],row[8],
+                                      row[9],row[10],row[11],
+                                      row[12],row[13],row[14],
+                                      row[15].capitalize(),row[16].capitalize(),row[17].capitalize(),
+                                      row[18].capitalize(),row[19].capitalize(),
+                                      row[20], row[21].capitalize(),row[22].capitalize(),
+                                      row[23].capitalize(),row[24].capitalize(),
+                                      row[25].capitalize(),row[26].capitalize(),
                                       row[27].capitalize(),row[28].capitalize(),row[29])
                     a.save()
                 except Exception as e:
